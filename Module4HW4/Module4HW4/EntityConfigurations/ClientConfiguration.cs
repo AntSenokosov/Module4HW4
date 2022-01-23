c332d8557c11d605c706deb85dd3dd5f51b65622
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-23 19:33:08 +0200

    Add ClientConfiguration

---
+++ Module4HW4/Module4HW4/EntityConfigurations/ClientConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW4.Entities;
+
+namespace Module4HW4.EntityConfigurations;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.ToTable("Client").HasKey(c => c.Id);
+        builder.Property(e => e.Id).HasColumnName("ClientId").ValueGeneratedOnAdd();
+        builder.Property(e => e.FirstName).IsRequired().HasColumnName("FirstName").HasMaxLength(50);
+        builder.Property(e => e.LastName).IsRequired().HasColumnName("LastName").HasMaxLength(50);
+        builder.Property(e => e.Email).IsRequired().HasColumnName("Email");
+        builder.Property(e => e.Phone).IsRequired().HasColumnName("Phone").HasMaxLength(15);
+    }
+}
