4df2ef30d6e8c72f3116f67bc195dafaaf4f2441
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-23 20:37:16 +0200

    Add date in entities Client and Project

--- Module4HW4/Module4HW4/EntityConfigurations/ClientConfiguration.cs
+++ Module4HW4/Module4HW4/EntityConfigurations/ClientConfiguration.cs
@@ -14,5 +14,49 @@ public class ClientConfiguration : IEntityTypeConfiguration<Client>
         builder.Property(e => e.LastName).IsRequired().HasColumnName("LastName").HasMaxLength(50);
         builder.Property(e => e.Email).IsRequired().HasColumnName("Email");
         builder.Property(e => e.Phone).IsRequired().HasColumnName("Phone").HasMaxLength(15);
+
+        builder.HasData(new List<Client>()
+        {
+            new Client()
+            {
+                Id = 1,
+                FirstName = "Vlad",
+                LastName = "Gricak",
+                Email = "v.g@gmail.com",
+                Phone = "+380663424343"
+            },
+            new Client()
+            {
+                Id = 2,
+                FirstName = "Stas",
+                LastName = "Nizik",
+                Email = "s.n@gmail.com",
+                Phone = "+380995323373"
+            },
+            new Client()
+            {
+                Id = 3,
+                FirstName = "Danil",
+                LastName = "Levinskiy",
+                Email = "d.l@gmail.com",
+                Phone = "+380673436442"
+            },
+            new Client()
+            {
+                Id = 4,
+                FirstName = "Vova",
+                LastName = "Tkachenko",
+                Email = "v.t@gmail.com",
+                Phone = "+380987543234"
+            },
+            new Client()
+            {
+                Id = 5,
+                FirstName = "Dmitriy",
+                LastName = "Pyatak",
+                Email = "d.p@gmail.com",
+                Phone = "+380992346342"
+            },
+        });
     }
 }
--- Module4HW4/Module4HW4/EntityConfigurations/ProjectConfiguration.cs
+++ Module4HW4/Module4HW4/EntityConfigurations/ProjectConfiguration.cs
@@ -17,5 +17,49 @@ public class ProjectConfiguration : IEntityTypeConfiguration<Project>
         builder.HasOne(p => p.Client)
             .WithMany(c => c.Projects)
             .HasForeignKey(h => h.ClientId);
+
+        builder.HasData(new List<Project>()
+        {
+            new Project()
+            {
+                Id = 1,
+                Name = "Project for study",
+                Budget = 500000,
+                StartedDate = new DateTime(2021, 12, 12),
+                ClientId = 1
+            },
+            new Project()
+            {
+                Id = 2,
+                Name = "Game",
+                Budget = 1200000,
+                StartedDate = new DateTime(2022, 5, 3),
+                ClientId = 2
+            },
+            new Project()
+            {
+                Id = 3,
+                Name = "Web application",
+                Budget = 78000,
+                StartedDate = new DateTime(2021, 6, 29),
+                ClientId = 1
+            },
+            new Project()
+            {
+                Id = 4,
+                Name = "Desktop application",
+                Budget = 230000,
+                StartedDate = new DateTime(2022, 1, 23),
+                ClientId = 4
+            },
+            new Project()
+            {
+                Id = 5,
+                Name = "Database development",
+                Budget = 10000,
+                StartedDate = new DateTime(2022, 2, 4),
+                ClientId = 3
+            }
+        });
     }
 }
