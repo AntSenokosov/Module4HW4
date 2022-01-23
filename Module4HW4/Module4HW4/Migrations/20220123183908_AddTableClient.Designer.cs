cce04d4e3ad530f2012ab918f27a0742c5a294c9
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-23 20:45:17 +0200

    Add mirgration AddTableClient

--- Module4HW4/Module4HW4/Migrations/20220123183908_AddTableClient.Designer.cs
+++ Module4HW4/Module4HW4/Migrations/20220123183908_AddTableClient.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Module4HW4;
 
@@ -11,9 +12,10 @@ using Module4HW4;
 namespace Module4HW4.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220123183908_AddTableClient")]
+    partial class AddTableClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
